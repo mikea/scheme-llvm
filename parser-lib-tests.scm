4d1a282964cc68afab51a54ad720fc75fee18b32
Mike Aizatsky <mike.aizatsky@gmail.com>
2008-06-17 19:55:41 +0400

    adding while-char

--- parser-lib-tests.scm
+++ parser-lib-tests.scm
@@ -4,35 +4,38 @@
 (load "parser-lib.scm")
 (import parser-lib)
 
+(test-group "nop"
+	    (test '(#t . 0) (parse-one nop "abc" 0)))
+
 (test-group "any-char"
-	    (test '("a" 1) (parse-one any-char "abc" 0))
-	    (test '("b" 2) (parse-one any-char "abc" 1))
-	    (test '("c" 3) (parse-one any-char "abc" 2))
+	    (test '("a" . 1) (parse-one any-char "abc" 0))
+	    (test '("b" . 2) (parse-one any-char "abc" 1))
+	    (test '("c" . 3) (parse-one any-char "abc" 2))
 	    (test #f (parse-one any-char "abc" 3))
 	    )
 
 (test-group "char"
-	    (test '("a" 1) ((char "a") "abc" 0))
+	    (test '("a" . 1) ((char "a") "abc" 0))
 	    (test #f ((char "b") "abc" 0))
 	    (test #f ((char "b") "abc" 3))
 	    )
 
 (test-group "seq"
-	    (test '(("a" "b") 2) ((seq (any-char) (any-char)) "abc" 0))
+	    (test '(("a" "b") . 2) ((seq (any-char) (any-char)) "abc" 0))
 	    (test #f ((seq (any-char) (any-char)) "abc" 2))
-	    (test '(("a" "b") 2) ((seq (char "a") (char "b")) "abc" 0))
+	    (test '(("a" "b") . 2) ((seq (char "a") (char "b")) "abc" 0))
 	    (test #f ((seq (char "b") (char "b")) "abc" 0))
 	    (test #f ((seq (char "a") (char "c")) "abc" 0)) 
 	    )
 
 (test-group "parser"
-	    (test '(#t 1) (let ((p (parser ((c1 <- (any-char))))))
+	    (test '(#t . 1) (let ((p (parser ((c1 <- (any-char))))))
 			    (p "abc" 0)))
-	    (test '(#t 2) (let ((p (parser ((c1 <- (any-char)) 
+	    (test '(#t . 2) (let ((p (parser ((c1 <- (any-char)) 
 					    (c2 <- (any-char)))
 					   )))
 			    (p "abc" 0)))
-	    (test '(#t 2) (let ((p (parser ((c1 <- (char "a")) 
+	    (test '(#t . 2) (let ((p (parser ((c1 <- (char "a")) 
 					    (c2 <- (any-char)))
 					   )))
 			    (p "abc" 0)))
@@ -40,10 +43,15 @@
 				       (c2 <- (any-char)))
 				      )))
 		       (p "dabc" 0)))
-	    (test '("ab" 2) (let ((p (parser ((c1 <- (any-char)) 
+	    (test '("ab" . 2) (let ((p (parser ((c1 <- (any-char)) 
 					      (c2 <- (any-char))
 					      (return (string-append c1 c2)))
 					     )))
 			    (p "abc" 0)))
 )
 
+(test-group "while-char"
+	    (test '("123" . 3) ((while-char digit?) "123abc" 0))
+	    (test '("123" . 3) ((while-char digit?) "123" 0))
+	    (test #f ((while-char digit?) "abc" 0))
+)
--- parser-lib.scm
+++ parser-lib.scm
@@ -1,21 +1,33 @@
 (require-extension syntax-case)
 
 (module parser-lib
-	(any-char char seq parse-one parser nop)
+	(any-char char seq parse-one parser nop
+		  while-char digit?)
+
+;; The module defines haskell-style parser combinators.
+;;
+;; A parser is a function of two arguments:
+;;   - string which is parsed
+;;   - offset to start parsing from
+;; Parser function returns either:
+;;   - #f if parsing was not successful
+;;   - a pair (parsing_result . new_starting_offset)
 
 (define (parse-one f s p) ((f) s p))
 
 (define (nop)
   (lambda (s p)
-    (list #t p)))
+    (cons #t p)))
 
+;; Create a parser which accepts any char
 (define (any-char) 
   (lambda (s p)
     (if (< p (string-length s))
-	(list (substring s p (+ p 1)) 
+	(cons (substring s p (+ p 1)) 
 	      (+ p 1))
 	#f)))
 
+;; Create a parser which accepts a given char
 (define (char c)
   (lambda (s p)
     (let ((r ((any-char) s p)))
@@ -25,35 +37,50 @@
       )
     ))
 
+;; Create a parser which accepts all chars while predicate is true
+(define (while-char predicate)
+  (lambda (s p)
+    (let ((len (string-length s)))
+      (let loop ((i p))
+	(if (and (< i len) (predicate (string-ref s i)))
+	    (loop (+ i 1))
+	    (if (eq? p i)
+		#f
+		(cons (substring s p i) i)))))))
+
 (define (seq p1 p2)
   (lambda (s p)
     (let ((r1 (p1 s p)))
       (if r1
-	  (let ((r2 (p2 s (cadr r1))))
+	  (let ((r2 (p2 s (cdr r1))))
 	    (if r2
-		(list (list (car r1) (car r2)) (cadr r2))
+		(cons (list (car r1) (car r2)) (cdr r2))
 		#f))
 	  #f)
       )))
 
 (define-syntax parser
   (syntax-rules ()
-    ((parser ()) (lambda (s p) (list #t p)))
+    ((parser ()) (lambda (s p) (cons #t p)))
     ((parser ((v <- head-parser) . tail)) 
      (lambda (s p) 
        (let* ((head-result (head-parser s p)))
 	 (if head-result
 	     (let* ((v (car head-result))
 		    (tail-parser (parser tail))
-		    (tail-pos (cadr head-result))
-		    (tail-result (tail-parser s tail-pos)))
-	       (list (car tail-result) (cadr tail-result)))
+		    (head-pos (cdr head-result))
+		    (tail-result (tail-parser s head-pos)))
+	       (cons (car tail-result) (cdr tail-result)))
 	     #f))
      ))
-    ((parser ((return e))) (lambda (s p) (list e p)))
+    ((parser ((return e))) (lambda (s p) (cons e p)))
 ))
 
-;; TBD: (matches "token"),  (while), (while-char space?), (choice)
+;; TBD: (matches "token"),  (while), (choice)
 
+;; Some useful predicates
+
+(define (digit? c)
+  (and (char<=? #\0 c) (char>=? #\9 c)))
 
 )
